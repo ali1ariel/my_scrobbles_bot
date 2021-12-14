82c55879b469e572733ba9b00cfc26c560b46f19
Alisson <aamachado@inf.ufsm.br>
2021-12-14 04:29:11 -0300

    fix

--- lib/my_scrobbles_bot/last_fm/artist.ex
+++ lib/my_scrobbles_bot/last_fm/artist.ex
@@ -109,38 +109,6 @@ defmodule MyScrobblesBot.LastFm.Artist do
     %{text: msg, parse_mode: "markdown", chat_id: message.chat_id}
   end
 
-  def myartist(message) do
-    %{
-      message: %{
-        from: %{
-          telegram_id: user_id,
-          first_name: user_first_name
-        },
-        reply_to_message: %{
-          from: %{
-            telegram_id: friend_user_id,
-            first_name: friend_first_name
-          }
-        }
-      }
-    } = message
-
-    %{last_fm_username: username} = MyScrobblesBot.Accounts.get_user_by_telegram_user_id!(user_id)
-
-    %{last_fm_username: friend_username} =
-      MyScrobblesBot.Accounts.get_user_by_telegram_user_id!(friend_user_id)
-
-    {:ok, track} = LastFm.get_recent_track(%{username: username})
-    {:ok, attrs} = LastFm.get_artist(%{track | username: friend_username})
-
-    query =
-      Map.merge(track, attrs)
-      |> Map.merge(%{with_photo?: true, user: user_first_name, friend: friend_first_name})
-
-    msg = LastFm.get_my_artist(query)
-    %{text: msg, parse_mode: "markdown", chat_id: message.chat_id}
-  end
-
   def artist_tracks(tracks, username) do
     Enum.map(tracks["track"] |> Enum.take(20), fn %{
                                                     "name" => track,
